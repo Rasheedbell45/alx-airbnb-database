404c2a7766559a04546abf9c9ab72c5380e58f0a
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-02 21:57:50 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -18,9 +18,14 @@ GROUP BY
     u.id, u.name, u.email;
 SELECT 
     property_id,
-    COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-FROM 
-    bookings
-GROUP BY 
-    property_id;
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT 
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM 
+        bookings
+    GROUP BY 
+        property_id
+) AS property_counts;
