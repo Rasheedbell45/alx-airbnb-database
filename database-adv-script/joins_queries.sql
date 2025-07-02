1ac9ad1e8aee480be3ead75b49bee24d9ba9a9d9
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-02 21:38:54 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,31 @@
-
+SELECT 
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.booking_date,
+    users.id AS user_id,
+    users.name,
+    users.email
+FROM 
+    bookings
+INNER JOIN 
+    users ON bookings.user_id = users.id;
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.id = reviews.property_id;
+    SELECT 
+    users.id AS user_id,
+    users.name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings ON users.id = bookings.user_id;
